c40f123c8e03f3333ca477f87498799034dcfc7b
Raqeeb Anjum <rraqeebanjum04@gmail.com>
2024-10-14 21:08:07 -0500

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Raqeeb Anjum on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
